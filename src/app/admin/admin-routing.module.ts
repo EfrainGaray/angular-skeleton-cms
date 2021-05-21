aeec7f9a94083db746b56b3ff92286eccc2afc40
Efrain Garay <egaray7@gmail.com>
2021-05-21 03:22:10 -0400

    feat(): Login admin html css (#1)
    * feat(): add normalize and create login html css
    
    * fix(): change sass to scss
    
    * feat(): add atomic design  architecture scss and dark mode
    
    * feat(): refactor login

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: 'sign',   loadChildren: () => import('./login/login.module').then( m => m.LoginModule ) },
+      { path: '**', redirectTo: 'sign' }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/admin/login/login.module.ts
+++ src/app/admin/login/login.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SignupComponent } from "@login/signup";
+import { ForgotComponent } from "@login/forgot";
+import { SigninComponent } from "@login/signin";
+import { FooterComponent, HeaderComponent } from "@login/shared";
 
 import { LoginRoutingModule } from './login-routing.module';
-import { ForgotComponent } from './forgot/forgot.component';
-import { SignupComponent } from './signup/signup.component';
-import { SigninComponent } from './signin/signin.component';
 
 
 @NgModule({
   declarations: [
     ForgotComponent,
     SignupComponent,
-    SigninComponent
+    SigninComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
