77543c26db317f9de6878e86b10a4e54722e4348
Efrain Garay <egaray7@gmail.com>
2021-05-21 03:20:45 -0400

    feat(): refactor login

--- src/app/admin/login/shared/header/header.component.ts
+++ src/app/admin/login/shared/header/header.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+
+declare function  setUpThemeToggle() :any;
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.sass']
+  styleUrls: ['./header.component.scss']
 })
+
+
+
 export class HeaderComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+    setUpThemeToggle()
   }
 
 }
