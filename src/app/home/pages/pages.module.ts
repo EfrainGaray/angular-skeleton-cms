44042a65c0fddd82eaa102d263bfe250f7f09a7d
Efrain Garay <egaray7@gmail.com>
2022-04-03 22:37:15 -0400

    feat(): add landing-page

--- src/app/home/pages/pages.module.ts
+++ src/app/home/pages/pages.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PagesRoutingModule } from './pages-routing.module';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+import { HeaderComponent } from '../shared/header/header.component';
+import { FooterComponent } from '../shared/footer/footer.component';
+
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LandingpageComponent
+  ],
   imports: [
     CommonModule,
-    PagesRoutingModule
+    PagesRoutingModule,
+    HeaderComponent,
+    FooterComponent 
   ]
 })
 export class PagesModule { }
